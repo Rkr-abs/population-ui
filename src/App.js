98628ee499741f88d712dd4cc01b184b43773a75
RamKumar <rramkumar1306.com>
2022-11-23 15:47:46 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { React } from 'react';
 import './App.scss';
+import Table from './components/table';
 
 const App = () =>
-	<div className="App" role="App">
-		Ready to start.
-	</div>;
+	<div className="App" role="App">{Table()}</div>;
 
 export default App;
